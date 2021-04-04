820a5a29e44ddb4aaf4d98acd40eabc87e1db152
OSN-DEV <osndev1973@gmail.com>
2021-04-04 18:23:22 +0900

    create skeleton

---
+++ UI/MyAnnotationMain/MyAnnotationMainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OsnCsLib.WPFComponent.Bind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAnnotationCopy.UI.MyAnnotationMain {
+    /// <summary>
+    /// アノテーションメイン画面のビューモデル
+    /// </summary>
+    class MyAnnotationMainViewModel : BaseBindable {
+    }
+}
---
+++ UI/MyAnnotationMain/MyAnnotationMainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyAnnotationCopy.UI.MyAnnotationMain {
+    /// <summary>
+    /// アノテーションメイン画面
+    /// </summary>
+    public partial class MyAnnotationMainWindow : Window {
+        public MyAnnotationMainWindow() {
+            InitializeComponent();
+        }
+    }
+}
