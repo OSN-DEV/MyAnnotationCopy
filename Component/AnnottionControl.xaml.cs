7360b739202f9f1226789a9a0280dc0bb967dd29
OSN-DEV <osndev1973@gmail.com>
2021-04-05 16:31:35 +0900

    カスタムコントロールを実装

--- Component/AnnottionControl.xaml.cs
+++ Component/AnnottionControl.xaml.cs
@@ -1,25 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OsnCsLib.WPFComponent.Bind;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyAnnotationCopy.Component {
     /// <summary>
-    /// AnnottionControl.xaml の相互作用ロジック
+    /// アノテーション操作コントロール
     /// </summary>
     public partial class AnnottionControl : UserControl {
+
+        #region Public Method
+        /// <summary>
+        /// 自動カウント使用有無
+        /// </summary>
+        public static readonly DependencyProperty IsUseIncrementProperty = RegisterProperty(nameof(IsUseIncrement), typeof(bool));
+        public bool IsUseIncrement {
+            get { return (bool)GetValue(IsUseIncrementProperty); }
+            set { SetValue(IsUseIncrementProperty, value); }
+        }
+
+        /// <summary>
+        /// 全角変換使用有無
+        /// </summary>
+        public static readonly DependencyProperty IsUseWideProperty = RegisterProperty(nameof(IsUseWide), typeof(bool));
+        public bool IsUseWide {
+            get { return (bool)GetValue(IsUseWideProperty); }
+            set { SetValue(IsUseWideProperty, value); }
+        }
+
+        /// <summary>
+        /// プレフィックス
+        /// </summary>
+        public static readonly DependencyProperty PrefixProperty = RegisterProperty(nameof(Prefix), typeof(string));
+        public string Prefix {
+            get { return (string)GetValue(PrefixProperty); }
+            set { SetValue(PrefixProperty, value); }
+        }
+
+        /// <summary>
+        /// 現在の番号ラベル
+        /// </summary>
+        public static readonly DependencyProperty CurrentNumberLabelProperty = RegisterProperty(nameof(Prefix), typeof(string));
+        public string CurrentNumberLabel {
+            get { return (string)GetValue(CurrentNumberLabelProperty); }
+            set { SetValue(CurrentNumberLabelProperty, value); }
+        }
+
+        /// <summary>
+        /// サフィックス
+        /// </summary>
+        public static readonly DependencyProperty SafixProperty = RegisterProperty(nameof(Safix), typeof(string));
+        public string Safix {
+            get { return (string)GetValue(SafixProperty); }
+            set { SetValue(SafixProperty, value); }
+        }
+
+        /// <summary>
+        /// コピーコマンド
+        /// </summary>
+        public DelegateCommand CopyCommand { set; get; }
+        #endregion
+
+        #region Constructor
         public AnnottionControl() {
             InitializeComponent();
+            this.CopyCommand = new DelegateCommand(CopyClick, () =>
+            (null != this.CurrentNumberLabel && 0 < this.CurrentNumberLabel.Length));
         }
+
+        static AnnottionControl() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnnottionControl), new FrameworkPropertyMetadata(typeof(AnnottionControl)));
+        }
+        #endregion
+
+
+        #region Private Method(Event)
+        /// <summary>
+        /// コピーボタン クリック時処理
+        /// </summary>
+        private void CopyClick() {
+            SetTextToClipboard($"{this.Prefix}{this.CurrentNumberLabel}{this.Safix}");
+        }
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// プロパティを登録する
+        /// </summary>
+        /// <param name="name">プロパティ名</param>
+        /// <param name="propertyType">プロパティの型</param>
+        private static DependencyProperty RegisterProperty(string name, Type propertyType) {
+            return DependencyProperty.Register(name, propertyType, typeof(AnnottionControl), new FrameworkPropertyMetadata());
+        }
+
+        /// <summary>
+        /// テキストをクリップボードにコピーする
+        /// </summary>
+        /// <param name="text">コピーするテキスト</param>
+        private static void SetTextToClipboard(string text) {
+            try {
+                Clipboard.Clear();
+                System.Threading.Thread.Sleep(100);
+                Clipboard.SetText(text, TextDataFormat.UnicodeText);
+            } catch {
+                try {
+                    Clipboard.SetDataObject(text);
+                } catch {
+                }
+            }
+        }
+        #endregion
+
     }
 }
