94e84e5d6261677f44407abb8a35b0c5c6f130d0
OSN-DEV <osndev1973@gmail.com>
2021-04-05 16:06:20 +0900

    データモデルを作成

--- Data/PreferenceData.cs
+++ Data/PreferenceData.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyAnnotationCopy.AppCommon;
+using OsnCsLib.WPFComponent.Bind;
+using System.Collections.ObjectModel;
 
 namespace MyAnnotationCopy.Data {
-    class PreferenceData {
+    /// <summary>
+    /// アプリケーションデータ データモデル
+    /// </summary>
+    internal class PreferenceData : BaseBindable {
+
+        #region Internal Property
+        /// <summary>
+        /// ウィンドウのX座標
+        /// </summary>
+        private double _x;
+        internal double X {
+            set { base.SetProperty(ref this._x, value); }
+            get { return this._x; }
+        }
+
+        /// <summary>
+        /// ウィンドウのY座標
+        /// </summary>
+        private double _y;
+        internal double Y {
+            set { base.SetProperty(ref this._y, value); }
+            get { return this._y; }
+        }
+
+        /// <summary>
+        /// 現在の番号
+        /// </summary>
+        private int _currentNumber;
+        internal int CurrentNumber {
+            set { base.SetProperty(ref this._currentNumber, value); }
+            get { return this._currentNumber; }
+        }
+
+        /// <summary>
+        /// 設定詳細
+        /// </summary>
+        private PreferenceDetailData[] _settingDetail = new PreferenceDetailData[Constants.MaxRowCount];
+        internal PreferenceDetailData[] SettingDetail {
+            set { base.SetProperty(ref this._settingDetail, value); }
+            get { return this._settingDetail; }
+        }
+        #endregion
     }
 }
--- Data/PreferenceDetailData.cs
+++ Data/PreferenceDetailData.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OsnCsLib.WPFComponent.Bind;
 
 namespace MyAnnotationCopy.Data {
-    class PreferenceDetailData {
+    /// <summary>
+    /// アプリケーションデータ(詳細) データモデル
+    /// </summary>
+    internal class PreferenceDetailData : BaseBindable {
+
+        #region Internal Property
+        /// <summary>
+        /// 自動カウント使用有無
+        /// </summary>
+        private bool _isUseIncrement;
+        internal bool IsUseIncrement {
+            set { base.SetProperty(ref this._isUseIncrement, value); }
+            get { return this._isUseIncrement; }
+        }
+
+        /// <summary>
+        /// 全角変換使用有無
+        /// </summary>
+        private bool _isUseWide;
+        internal bool IsUseWide {
+            set { base.SetProperty(ref this._isUseWide, value); }
+            get { return this._isUseWide; }
+        }
+
+        /// <summary>
+        /// プレフィックス
+        /// </summary>
+        private string _prefix;
+        internal string Prefix {
+            set { base.SetProperty(ref this._prefix, value); }
+            get { return this._prefix; }
+        }
+
+        /// <summary>
+        /// サフィックス
+        /// </summary>
+        private string _safix;
+        internal string Safix {
+            set { base.SetProperty(ref this._safix, value); }
+            get { return this._safix; }
+        }
+        #endregion
+
     }
 }
